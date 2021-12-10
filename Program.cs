0cf0fcbb355cc18b0e241c1ba12433a96f487970
hypeartist <hypeartist@gmail.com>
2021-12-11 01:29:29 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.MapBlazorHub();
+app.MapFallbackToPage("/EntryPage");
+
+await app.RunAsync();
